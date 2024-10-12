9dde5199f7dc98018e936b5e03f65c003cb26665
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-10-12 02:08:41 -0400

    Page title

--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import { useState } from "react";
 import fs from "fs";
+import Head from "next/head";
 
 const geistSans = localFont({
     src: "./fonts/GeistVF.woff",
@@ -52,6 +53,9 @@ export default function Home({ envs }) {
         <div
             className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
         >
+            <Head>
+                <title>Environments</title>
+            </Head>
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
                 <ol className="list-inside  text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                     {envs.map((env, index) => (
