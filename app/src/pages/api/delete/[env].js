9bc853019dc622bb74804949d6b155591e8f71b5
Peter Sukhenko <7243796+pezzu@users.noreply.github.com>
2024-10-14 01:07:43 -0400

    Invoking GH API to start deployment

--- app/src/pages/api/delete/[env].js
+++ app/src/pages/api/delete/[env].js
@@ -1,4 +1,17 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     console.log("Deleting " + req.query.env);
-    res.status(200).json({ name: "John Doe" });
+    const response = await fetch(
+        `https://api.github.com/repos/pezzu/scc-infra/actions/workflows/delete.yaml/dispatches`,
+        {
+            method: "POST",
+            headers: {
+                Accept: "application/vnd.github+json",
+                Authorization: `Bearer ${process.env.SCC_INFRA_TOKEN}`,
+                "X-GitHub-Api-Version": "2022-11-28",
+            },
+            body: JSON.stringify({ ref: "main", inputs: { environment: req.query.env } }),
+        }
+    );
+    console.log("GH Action response: " + response.ok);
+    res.status(200).json({ ok: true });
 }
